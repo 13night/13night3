159bf94d854cfe9f50b5cce1a54f48e97394d62b
13night <13night@land.ru>
2014-05-29 11:30:36 +0400

    KSKInvest

---
+++ KSK_APP/index.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    "use strict";
+
+    var AutoWashApp = window.AutoWashApp = { };
+    
+    // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
+    DevExpress.devices.current({ platform: "generic" });
+
+    $(function() {
+        AutoWashApp.app = new DevExpress.framework.html.HtmlApplication(
+		{
+            namespace: AutoWashApp,
+            navigationType: 'empty',
+        });
+        
+        AutoWashApp.app.router.register(":view", { view: "home" });
+        AutoWashApp.app.navigate();
+    });
+    
+})();
